800da5e8054aabba02596b2ef324539f2310f0a6
Ziyi Yang <ziyiyang8@gmail.com>
2013-12-08 15:51:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MediaMash::Application.config.secret_key_base = '6e21bde42ec02c2cc31d773052802b3c90447ecd12cd225a3eb2dbf5f450a30285bea5c2b78ff6b9720f9d011cdba9d52992d67123b95bd0454b6b27bd727bde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MediaMash::Application.config.session_store :cookie_store, key: '_media-mash_session'
