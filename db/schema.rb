c4238982007d05be0f6e1afe69c9146f8cee83e5
Ziyi Yang <ziyiyang8@gmail.com>
2013-12-08 18:12:21 -0800

    album has-many photo relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209020404) do
+ActiveRecord::Schema.define(version: 20131209021146) do
 
   create_table "albums", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131209020404) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "album_id"
   end
 
 end
