36e533fb0da256912abdb59ffdc64818d96287a9
Ziyi Yang <ziyiyang8@gmail.com>
2013-12-14 13:33:46 -0800

    click on thumbnail gives full size image

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -26,21 +26,25 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
+
   #
   # def scale(width, height)
   #   # do something
   # end
-
   # Create different versions of your uploaded files:
   version :thumb do
      process :resize_to_limit => [200, 200]
   end
 
+  version :original do
+    process :resize_to_fit => [1920, 1080]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
