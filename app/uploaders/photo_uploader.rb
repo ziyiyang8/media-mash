6361994550ac000f62995b51494d41711142bc8d
Ziyi Yang <ziyiyang8@gmail.com>
2013-12-22 18:08:58 -0800

    added css for photo display

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -33,7 +33,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_limit => [200, 200]
+     process :resize_to_fill => [200, 200]
   end
 
   version :original do
