6d15ebe1287524bcef0998e4edd584cc2a63e392
Ziyi Yang <ziyiyang8@gmail.com>
2014-01-27 10:21:07 -0800

    set up s3 for image uploads

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'fancybox2-rails'
 
 gem 'rmagick'
 gem 'carrierwave'
+gem 'fog', '~> 1.3.1'
 
 # Use sqlite3 as the database for Active Record
 group :test, :development do
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+CarrierWave.configure do |config|
+	config.fog_credentials = {
+		provider: "AWS",
+		aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+		aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+		region: "us-west-1"
+	}
+	config.fog_directory = ENV["AWS_S3_BUCKET"]
+
+	if Rails.env.test? || Rails.env.cucumber?
+		config.storage = :file
+		config.enable_processing = false
+		config.root = "#{Rails.root}/tmp"
+	else
+		config.storage = :fog
+	end
+
+	config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
+end
